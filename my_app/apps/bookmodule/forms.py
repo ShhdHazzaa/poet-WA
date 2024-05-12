8a297f30881bd870a7c887141fecaf2be702cfcc
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-12 08:02:55 +0300

    Update forms.py

--- my_app/apps/bookmodule/forms.py
+++ my_app/apps/bookmodule/forms.py
@@ -1,1 +1,29 @@
+from django import forms
+from .models import Poet, Poem
+
+class PoetForm(forms.ModelForm):
+    class Meta:
+        model = Poet
+        fields = ['name', 'bio']
+        labels = {
+            'name': 'Poet Name',
+            'bio': 'Biography',
+        }
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class PoemForm(forms.ModelForm):
+    class Meta:
+        model = Poem
+        fields = ['title', 'content', 'poet']
+        labels = {
+            'title': 'Title',
+            'content': 'Content',
+            'poet': 'Poet',
+        }
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 5}),
+        }
+ 
  
