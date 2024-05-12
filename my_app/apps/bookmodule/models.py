c2d0e98a314374d797b1d3e27b63b9d3f2a3a8c0
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-12 08:02:25 +0300

    Update models.py

--- my_app/apps/bookmodule/models.py
+++ my_app/apps/bookmodule/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-class Book(models.Model):
-    title = models.CharField(max_length = 50)
-    author = models.CharField(max_length = 50)
-    price = models.FloatField(default = 0.0)
-    edition = models.SmallIntegerField(default = 1)
+
+class Poet(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    
+    
+    class Meta:
+        app_label = 'bookmodule'
+    def __str__(self):
+        return self.name
+
+class Poem(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    poet = models.ForeignKey(Poet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
