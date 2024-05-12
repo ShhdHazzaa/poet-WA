ddb6dfddfaceeeb4cd9cf54d6d89db1c53b60e29
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-12 08:01:57 +0300

    Update views.py

--- my_app/apps/bookmodule/views.py
+++ my_app/apps/bookmodule/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Poet, Poem
 
-def index(request):
-    #study the req
-    return render(request, 'bookmodule/index.html')
-     
+def poet_list(request):
+    poets = Poet.objects.all()
+    return render(request, 'bookmodule/poet_list.html', {'poets': poets})
+
+def poet_detail(request, pk):
+    poet = Poet.objects.get(pk=pk)
+    return render(request, 'bookmodule/poet_detail.html', {'poet': poet})
+
+def poem_detail(request, pk):
+    poem = Poem.objects.get(pk=pk)
+    return render(request, 'bookmodule/poem_detail.html', {'poem': poem})
