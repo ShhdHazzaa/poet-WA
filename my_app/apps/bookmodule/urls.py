56f6d2080730a8eb85517d69892d0bac41faebce
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-12 09:41:49 +0300

    Update urls.py

--- my_app/apps/bookmodule/urls.py
+++ my_app/apps/bookmodule/urls.py
@@ -1,9 +1,9 @@
-
-from django.urls import path, re_path
-from apps.bookmodule import views
-
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.home, name='home'),
+    path('poem/<int:pk>/', views.poem_detail, name='poem_detail'),
+    path('poet/<int:pk>/', views.poet_detail, name='poet_detail'),
+    path('poet/', views.poet_list, name='poet_list'),
 ]
