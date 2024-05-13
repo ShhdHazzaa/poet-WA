68ba0f5a2208bb9477465e18095de8e60ebff5df
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-13 16:32:50 +0300

    Update urls.py

--- my_app/apps/bookmodule/urls.py
+++ my_app/apps/bookmodule/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 from .views import PoemListView, PoemDetailView, PoetListView, PoetDetailView
 
 urlpatterns = [
@@ -6,4 +7,7 @@ urlpatterns = [
     path('poems/<int:pk>/', PoemDetailView.as_view(), name='poem_detail'),
     path('poets/', PoetListView.as_view(), name='poet_list'),
     path('poets/<int:pk>/', PoetDetailView.as_view(), name='poet_detail'),
-]
+    path('admin/', admin.site.urls),
+    path('poems/', include('bookmodule.urls')),
+    path('', RedirectView.as_view(url='/poems/', permanent=True)),
+    
