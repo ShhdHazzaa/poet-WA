5a470e76b93bea7be247f860e73b7f012ea45efa
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-14 16:05:29 +0300

    Update urls.py

--- my_app/apps/bookmodule/urls.py
+++ my_app/apps/bookmodule/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include, re_path
-from django.views.generic import RedirectView
-from .views import PoemListView, PoemDetailView, PoetListView, PoetDetailView
+from django.urls import path
+from .views import (PoemListView, PoemDetailView, PoemCreateView, 
+                    PoemUpdateView, PoemDeleteView, PoetListView, PoetDetailView)
 
 urlpatterns = [
-    path('poems/', PoemListView.as_view(), name='poem_list'),
-    path('poems/<int:pk>/', PoemDetailView.as_view(), name='poem_detail'),
+    path('', PoemListView.as_view(), name='poem_list'),
+    path('poem/<int:pk>/', PoemDetailView.as_view(), name='poem_detail'),
+    path('poem/new/', PoemCreateView.as_view(), name='poem_create'),
+    path('poem/<int:pk>/edit/', PoemUpdateView.as_view(), name='poem_edit'),
+    path('poem/<int:pk>/delete/', PoemDeleteView.as_view(), name='poem_delete'),
     path('poets/', PoetListView.as_view(), name='poet_list'),
-    path('poets/<int:pk>/', PoetDetailView.as_view(), name='poet_detail'),
-    path('admin/', admin.site.urls),
-    path('poems/', include('bookmodule.urls')),
-    path('', RedirectView.as_view(url='/poems/', permanent=True)),
-    
+    path('poet/<int:pk>/', PoetDetailView.as_view(), name='poet_detail'),
+]
