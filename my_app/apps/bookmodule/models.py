9f293153d81cedbeeea7f808ce8da73b6c815521
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-14 16:03:47 +0300

    Update models.py

--- my_app/apps/bookmodule/models.py
+++ my_app/apps/bookmodule/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 class Poet(models.Model):
     name = models.CharField(max_length=100)
-    birth_date = models.DateField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Poem(models.Model):
     title = models.CharField(max_length=200)
-    publication_date = models.DateField()
-    poet = models.ForeignKey(Poet, on_delete=models.CASCADE)
+    body = models.TextField()
+    poet = models.ForeignKey(Poet, on_delete=models.CASCADE, related_name='poems')
+    date_posted = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
