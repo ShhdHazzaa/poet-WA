b0a205aac0b53aa57b6a8b32efe4e46b8abf2975
ShhdHazzaa <159417055+ShhdHazzaa@users.noreply.github.com>
2024-05-15 14:47:26 +0300

    Update models.py

--- my_app/apps/bookmodule/models.py
+++ my_app/apps/bookmodule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Poet(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +16,6 @@ class Poem(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('poem_detail', kwargs={'pk': self.pk})
